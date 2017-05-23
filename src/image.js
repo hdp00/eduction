33738075d6c8300c41ae7f5f82822df1c3a82545
hdp <houdongping@gmail.com>
2017-05-23 09:32:17 +0800

    init version

---
+++ src/image.js
@@ -0,0 +1,7 @@
+import imageTrue from './images/true.png'
+import imageFalse from './images/false.png'
+import image0 from './images/0.PNG'
+import image2 from './images/2.PNG'
+import image3 from './images/3.PNG'
+
+export {imageTrue, imageFalse, image0, image2, image3}
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import registerServiceWorker from './registerServiceWorker';
+import {App} from './app'
+
+const a = new App();
+a.run();
+registerServiceWorker();
